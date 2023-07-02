c119ffd2e63311ea8ab92226b6c063ea2a1f4b37
aponterob <roberthaponte24@gmail.com>
2023-07-02 13:38:38 -0400

    mensaje del commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(function(item){
+    item.addEventListener('click',function(e){
+        const currentItem = document.querySelector('.active');
+        currentItem.classList.remove('active');
+        e.target.classList.add('active');
+    });
+});
+
+const changeFontBtn = document.querySelector('#change-font');
+changeFontBtn.addEventListener('click',function(){
+    document.body.classList.toggle('change-font')
+})
